1c99574452ade4d654fcc02ffa3c2714d80b9f1e
Kasper Laurberg <kasperlaurberg@gmail.com>
2014-09-23 14:13:22 +0200

    Added example of using Java code from scala

--- Code/KMeans/src/main/scala/MainScala.scala
+++ Code/KMeans/src/main/scala/MainScala.scala
@@ -1,9 +1,21 @@
+import kmeans.datageneration.DataGenerator
+import kmeans.datageneration.DataGenerator
+import kmeans.model.Vector
+import kmeans.parsing.DataParser
+import kmeans.partitioning.Partitioner
+import kmeans.partitioning.Partitioning
+import java.util.ArrayList
+import java.util.List
+
 /**
  * Created by Kasper on 23-09-2014.
  */
 object MainScala {
 
   def main(args: Array[String]) {
+    val vectors = DataGenerator.generateData()
+    val partitioning = new Partitioner[Vector].partition(vectors,5);
+
     println("Hello, world!")
   }
 }
