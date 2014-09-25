753aed71dccff1264c276ed1d9df2573eaae4ee3
Kasper Laurberg <kasperlaurberg@gmail.com>
2014-09-25 16:39:49 +0200

    Added section on mutual exclusion

--- Code/Code_examples/Race_condition/src/MainLock.java
+++ Code/Code_examples/Race_condition/src/MainLock.java
@@ -1,21 +1,32 @@
-public class Main {
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by Kasper on 25-09-2014.
+ */
+public class MainLock {
     private static int number = 10;
     private static int result = 0;
+    private final static Lock lock = new ReentrantLock();
 
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
+                lock.lock();
                 if (number == 10){
                     Thread.yield();
                     result = number * 3;
                 }
+                lock.unlock();
             }
         });
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
-               number = 20;
+                lock.lock();
+                number = 20;
+                lock.unlock();
             }
         });
         t1.start(); t2.start();
@@ -23,7 +34,3 @@ public class Main {
         System.out.println("Result is: "+result);
     }
 }
-
-
-
-
