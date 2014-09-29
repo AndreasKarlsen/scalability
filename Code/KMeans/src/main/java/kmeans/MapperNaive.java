563ff0e47070f01f9a2f8f12fab830d78526fee1
Airbagjoe <kasperlaurberg@gmail.com>
2014-09-29 20:40:52 +0200

    Added naive implementation for java threads kmeans

---
+++ Code/KMeans/src/main/java/kmeans/MapperNaive.java
@@ -0,0 +1,39 @@
+package kmeans;
+
+import kmeans.clustering.Clustering;
+import kmeans.clustering.ClusteringService;
+import kmeans.model.Vector;
+
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by Kasper on 29-09-2014.
+ */
+public class MapperNaive implements  Runnable{
+
+    private Semaphore semaphore;
+    private ReentrantLock lock;
+    private List<Vector> data;
+    private Clustering clustering;
+
+    public MapperNaive(Semaphore semaphore, ReentrantLock lock, List<Vector> data, Clustering clustering) {
+        this.semaphore = semaphore;
+        this.lock = lock;
+        this.data = data;
+        this.clustering = clustering;
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println("Thread: "+Thread.currentThread().getId()+" started.");
+        ClusteringService.ClusterKMeansNaive(clustering,data,lock);
+        System.out.println("Thread: "+Thread.currentThread().getId()+" done clustering.");
+        //Hand off data
+        semaphore.release(1);
+        System.out.println("Thread: "+Thread.currentThread().getId()+" done delivering.");
+    }
+}
---
+++ Code/KMeans/src/main/java/kmeans/ReducerNaive.java
@@ -0,0 +1,53 @@
+package kmeans;
+
+import kmeans.clustering.Cluster;
+import kmeans.clustering.Clustering;
+
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by Kasper on 29-09-2014.
+ */
+public class ReducerNaive implements Runnable{
+
+    private Semaphore semaphore;
+    private int nrMappers;
+    private Clustering clustering;
+
+
+
+    public ReducerNaive(Semaphore semaphore, ReentrantLock lock, int nrMappers, Clustering clustering) {
+        this.semaphore = semaphore;
+        this.nrMappers = nrMappers;
+        this.clustering = clustering;
+    }
+
+    public Clustering getClustering() {
+        return clustering;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Reducer started");
+
+        int recieved = 0;
+        try {
+            while (recieved != nrMappers) {
+                semaphore.acquire(1);
+                recieved++;
+                System.out.println("Reducer recieved nr: "+recieved);
+            }
+
+            for (Cluster c :clustering.getClusters()){
+                c.calcMean();
+            }
+
+        }catch (InterruptedException ex){
+
+        }
+
+        System.out.println("Reducer finished");
+    }
+}
